9cb9f665712ce6e0919b4ac5aef85cf0940c87eb
Asepsukandar22 <asep.sukandar_ti23@nusaputra.ac.id>
2024-03-28 13:15:00 +0700

    update

---
+++ application/logs/log-2024-03-28.php
@@ -0,0 +1,7 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2024-03-28 04:06:17 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: YES) C:\xampp\htdocs\smkdb\system\database\drivers\mysqli\mysqli_driver.php 211
+ERROR - 2024-03-28 04:06:17 --> Unable to connect to the database
+ERROR - 2024-03-28 04:08:46 --> 404 Page Not Found: Media_library/images
+ERROR - 2024-03-28 05:00:40 --> 404 Page Not Found: SMK%20DOA%20BANGSA/index
+ERROR - 2024-03-28 05:04:08 --> 404 Page Not Found: reference/Student_status/index
